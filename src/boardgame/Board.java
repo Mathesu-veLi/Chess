95c131cbff821cd529f7df818039330690ac88d7
Matheus <matheuslevit@gmail.com>
2024-06-20 18:27:28 -0300

    applys defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -22,15 +22,24 @@ public class Board {
     return columns;
   }
 
-  public Piece piece (int row, int columns) {
-    return pieces[row][columns];
+  public Piece piece (int row, int column) {
+    if (!positionExists(row, column)) {
+      throw new BoardException("Position not on the board");
+    }
+    return pieces[row][column];
   }
 
   public Piece piece (Position position) {
+    if (!positionExists(position)) {
+      throw new BoardException("Position not on the board");
+    }
     return pieces[position.getRow()][position.getColumn()];
   }
 
   public void placePiece (Piece piece, Position position) {
+    if (thereIsAPiece(position)) {
+      throw new BoardException("There is already a piece on position " + position);
+    }
     pieces[position.getRow()][position.getColumn()] = piece;
     piece.position = position;
   }
@@ -44,6 +53,9 @@ public class Board {
   }
 
   public boolean thereIsAPiece (Position position) {
+    if (!positionExists(position)) {
+      throw new BoardException("Position not on the board");
+    }
     return piece(position) != null;
   }
 }
